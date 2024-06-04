82d6385f0d6751203534a14f9c3c23e3c8cf40c1
MJ 42 <mingjuntan93@gmail.com>
2024-06-04 17:56:01 +0800

    check printf behaviour

--- test1.c
+++ test1.c
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   test1.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mintan <mintan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 00:48:41 by mintan            #+#    #+#             */
-/*   Updated: 2024/06/04 00:48:41 by mintan           ###   ########.fr       */
+/*   Updated: 2024/06/04 17:55:25 by mintan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
 int main() {
-    int number = 123;
-    float pi = 3.14159;
+    int		number = 123;
+    float	pi = 3.14159;
+	char 	c = 'C';
+	char	*str = "this is a string";
+	void	*ptr;
 
+
+	
     printf("|%10d|\n", number);  // Right-justified, width 10
     printf("|%-10d|\n", number); // Left-justified, width 10
     printf("|%+10d|\n", number); // Right-justified with sign, width 10
@@ -26,7 +31,29 @@ int main() {
     printf("|%10.2f|\n", pi);    // Floating-point, width 10, 2 decimal places
     printf("|%010.2f|\n", pi);   // Zero-padded, width 10, 2 decimal places
     printf("|%+10.2f|\n", pi);   // Right-justified with sign, width 10, 2 decimal places
-	printf("|%+10.4f|\n", pi);   // Right-justified with sign, width 10, 4 decimal places
+	printf("|%+10.4f|\n\n", pi);   // Right-justified with sign, width 10, 4 decimal places
+	
+	ptr = &c;
+	printf("Char: %c\n", c);					// Print char
+	printf("Str: %s\n", str);					// Print str
+	printf("Ptr: %p\n", ptr);					// Print ptr
+	printf("Dec: %d\n", number);				// Print decimal (base 10) number
+	printf("Int: %i\n", number);				// Print an integer in base 10 should be the same as %d for printf
+	printf("Unsigned dec: %u\n", number);		// Print an unsigned decimal (base 10) number
+	printf("Unsigned dec: %u\n", -1);			// Print an unsigned decimal (base 10) number
+	printf("Unsigned dec: %u\n", -2);			// Print an unsigned decimal (base 10) number
+	printf("Unsigned dec: %u\n", -3);			// Print an unsigned decimal (base 10) number
+	printf("Unsigned dec: %u\n", -2147483647);	// Print an unsigned decimal (base 10) number
+
+
+	printf("Hex lowercase: %x\n", 127);		// Print hexadecimal (base 16) lowercase
+	printf("Hex uppercase: %X\n", 127);		// Print hexadecimal (base 16) uppercase
+	printf("Hex uppercase: %X\n", 127);		// Print hexadecimal (base 16) uppercase
+
+
+	printf("Percentage sign: %%\n");		// Print % sign
+
+
 
     return 0;
 }
